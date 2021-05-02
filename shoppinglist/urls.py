45951e9829d02f5d5ed1e973dc363f732a127541
aalrakabi <alrakabiawfa@gmail.com>
2021-05-01 23:25:24 -0700

    Implemented CRUD functionality. Need to work on better user interface.

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import ShoppinglistListView, ShoppinglistDetailView
+from .views import (
+	ShoppinglistListView, 
+	ShoppinglistDetailView, 
+	ShoppinglistCreateView, 
+	ShoppinglistUpdateView,
+	ShoppinglistDeleteView,
+)
+
 urlpatterns = [
+	path('post/<int:pk>/delete/', ShoppinglistDeleteView.as_view(), name='post_delete'),
+	path('post/<int:pk>/edit/', ShoppinglistUpdateView.as_view(), name='post_edit'),
+	path('post/new/', ShoppinglistCreateView.as_view(), name='post_new'), 
 	path('post/<int:pk>/', ShoppinglistDetailView.as_view(), name='post_detail'),
 	path('', ShoppinglistListView.as_view(), name='home'),
 ]
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class ShoppinglistListView(ListView):
 	model = Post
 	template_name = 'home.html'
 
-class ShoppinglistDetailView(DetailView): # new
+class ShoppinglistDetailView(DetailView): 
 	model = Post
 	template_name = 'post_detail.html'
+
+class ShoppinglistCreateView(CreateView): 
+	model = Post
+	template_name = 'post_new.html'
+	fields = ['title', 'author', 'body']
+
+class ShoppinglistUpdateView(UpdateView): 
+	model = Post
+	template_name = 'post_edit.html'
+	fields = ['title', 'body']
+
+class ShoppinglistDeleteView(DeleteView): # new
+	model = Post
+	template_name = 'post_delete.html'
+	success_url = reverse_lazy('home')
